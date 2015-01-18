4a46eec52979b44ddfbb2b0d9749363058101f05
Mathilde Mouw <nihaomouw@mac.com>
2015-01-17 18:30:08 -0800

    Show item text in edit activity

--- app/src/main/java/com/mathildemouw/simpletodo/SimpleTodoActivity.java
+++ app/src/main/java/com/mathildemouw/simpletodo/SimpleTodoActivity.java
@@ -57,7 +57,7 @@ public class SimpleTodoActivity extends ActionBarActivity {
         public void onItemClick(AdapterView<?> adapter,
                                 View item, int pos, long id) {
             Intent intent = new Intent(SimpleTodoActivity.this, EditActivity.class);
-            intent.putExtra("item", "placeholder");
+            intent.putExtra("item", items.get(pos));
             startActivity(intent);
             }
         });
